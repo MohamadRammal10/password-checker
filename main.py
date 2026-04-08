81fd14eecbd8f11969e84f280483fd3763bf7bbd
MohamadRammal10 <mohamad.rammal.mr@gmail.com>
2026-04-09 01:58:38 +0200

    Ajout fonction de calcul du score

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import re
+
+def calculate_score(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if re.search(r"[A-Z]", password) :
+        score += 1
+    if re.search(r"[a-z]", password) :
+        score += 1
+    if re.search(r"[0-9]", password) :
+        score += 1
+    if re.search(r"[^a-zA-Z0-9]", password) :
+        score += 1
+
+    return score
+
+
 password = input("Entrez votre mot de passe : ")
-print("Mot de passe recu : ", password)
+score = calculate_score(password)
+
+print("Score : ", score)
